f2a8867175fbd722dd4b3656381de3d87805416f
panaki-cyber <gillotpanaki@gmail.com>
2025-10-20 13:14:23 +0000

    Feat : add betty and file 1-compiler

--- Betty/betty.sh
+++ Betty/betty.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Simply a wrapper script to keep you from having to use betty-style
+# and betty-doc separately on every item.
+# Originally by Tim Britton (@wintermanc3r), multiargument added by
+# Larry Madeo (@hillmonkey)
+
 BIN_PATH="/usr/local/bin"
 BETTY_STYLE="betty-style"
 BETTY_DOC="betty-doc"
