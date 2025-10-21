aebab38bf7f575eaa7144df928fb4d7856bf1978
panaki-cyber <gillotpanaki@gmail.com>
2025-10-21 08:30:23 +0000

    Betty tu rend fou

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,6 +6,6 @@ Return: Always 0 (Success)
 */
 int main(void)
 {
-puts.c("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
