a177e42dc65176be034d23e67ea28c09eae7715b
panaki-cyber <gillotpanaki@gmail.com>
2025-10-21 08:10:33 +0000

    Fix : Update script for the file 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
-int main (void)
+#include <stdio.h>s
+
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
