19c03477b964d022453bcd162cb73e4c59a0dbda
panaki-cyber <gillotpanaki@gmail.com>
2025-10-22 09:40:40 +0000

    Fix : correct the output of the program

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,15 +15,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("%d the string and is greater than 5\n", n);
+	printf("%d the string and is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d the string and is 0\n", n);
+	printf("%d the string and is 0\n", n);
 	}
 	else (n < 6)
 	{
-		printf("%d the string and is less than 6 and not 0\n", n);
+	printf("%d the string and is less than 6 and not 0\n", n);
 	}
+	
 	return (0);
 }
