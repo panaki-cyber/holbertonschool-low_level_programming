e0e90b9fbcbb0911d7ae6339c67108537081dc6e
panaki-cyber <gillotpanaki@gmail.com>
2025-10-22 09:00:45 +0000

    Fix : Update program

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
-gcc -o positive_or_negative
--Wall | -Werror | -Wextra | -pedantic
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+**/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	std=gnu89 0-positive_or_negative.c 
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+
 	return (0);
 }
