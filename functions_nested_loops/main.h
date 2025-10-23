847ac18601c26440626dd1a25e26fdc11d8f0fdd
panaki-cyber <gillotpanaki@gmail.com>
2025-10-23 17:33:56 +0000

    Fix: Change variable

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,13 +13,5 @@ int _abs(int n);
 
 
 int add(int a, int b);
-int main(void)
 void print_to_98(int n);
-{
-    print_to_98(0);
-    print_to_98(111);
-    print_to_98(98);
-    return 0;
-}
-
 #endif
