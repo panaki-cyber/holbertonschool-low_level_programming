eaa0ecce908479bb1c0620b16ef97f224ad199c2
panaki-cyber <gillotpanaki@gmail.com>
2025-10-27 13:41:14 +0000

    Feat : create a function that check for digit 0 trough 9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* 1-isdigit.c - Check for digit 
+* @c: the character to check
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
