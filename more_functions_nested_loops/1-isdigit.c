ed3a7e284a86e12597b96470a2d60bc1a394d0a6
panaki-cyber <gillotpanaki@gmail.com>
2025-10-27 13:43:04 +0000

    Fix : correct output of program

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* 1-isdigit.c - Check for digit 
-* @c: the character to check
-*
-* Return: 1 if c is uppercase, 0 otherwise
-*/
+ * 1-isdigit.c - Check for digit 
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isdigit(int c);
 {
 	if (c >= '0' && c <= '9')
