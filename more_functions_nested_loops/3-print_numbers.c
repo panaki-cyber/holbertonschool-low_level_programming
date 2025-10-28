8a65912844fa908ede1ad8684b5a11c6e75bf2e9
panaki-cyber <gillotpanaki@gmail.com>
2025-10-28 14:25:20 +0000

    Fix : update script for the function that prints number

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- *print_number - prints the numbers from 0 to 9
+ * print_numbers - prints the numbers from 0 to 9
  *
- *Return: void
+ * Return: void
  */
-void print_numbers(void);
+void print_numbers(void)
 {
+	char c;
+
 	for (c = '0'; c <= '9'; c++)
 		_putchar(c);
 
-	_puthcar('\n');
+	_putchar('\n');
 }
