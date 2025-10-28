ce88fe188e0414c93d85a3ce2503369a4bb58c2a
panaki-cyber <gillotpanaki@gmail.com>
2025-10-28 14:23:07 +0000

    Feat a function that prints the number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_number - prints the numbers from 0 to 9
+ *
+ *Return: void
+ */
+void print_numbers(void);
+{
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+
+	_puthcar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _puthcar(char c);
+void print_numbers(void);
 
 #endif
