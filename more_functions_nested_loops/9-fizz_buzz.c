ddd15b427b604b718584648e138ad5995957533c
panaki-cyber <gillotpanaki@gmail.com>
2025-10-29 08:40:37 +0000

    Feat : add a function that create a triangle

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ *        but replaces multiples of 3 and 5 with Fizz/Buzz/FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+
+        if (i < 100)
+            printf(" ");
+    }
+    printf("\n");
+
+    return (0);
+}
