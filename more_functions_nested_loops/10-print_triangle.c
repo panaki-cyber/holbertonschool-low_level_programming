278da6f46ad50efa6ab69b56b6be2fdea81c1b7f
panaki-cyber <gillotpanaki@gmail.com>
2025-10-29 08:48:51 +0000

    Fix : correct output of the script

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,25 @@
  */
 void print_triangle(int size)
 {
-    int i, j, k;
+	int i, j, k;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 1; i <= size; i++)
-        {
-            for (j = size - i; j > 0; j--)
-            {
-                _putchar(' ');
-            }
-            for (k = 0; k < i; k++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
