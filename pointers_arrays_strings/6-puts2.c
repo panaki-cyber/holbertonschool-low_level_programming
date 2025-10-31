9e8d9da24c7d71e6e02f8e615b7019cfab05419d
panaki-cyber <gillotpanaki@gmail.com>
2025-10-31 17:45:21 +0000

    Feat: Add function to print every other character of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2; /* skip the next character */
+	}
+
+	_putchar('\n');
+}
