62cb1d1a0c4c9437ad43924cef7e6e6603bbde66
panaki-cyber <gillotpanaki@gmail.com>
2025-10-31 18:39:21 +0000

    Fix: correct the script of the new function.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point to test _strcmp
+ *
+ * Description: Compares two strings using _strcmp and prints
+ *              whether the first string is less than, equal to,
+ *              or greater than the second string.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *str1 = "Hello";
+	char *str2 = "Helium";
+	int res;
+
+	int res = _strcmp(str1, str2);
+
+	if (res < 0)
+		printf("str1 is less than str2\n");
+	else if (res > 0)
+		printf("str1 is greater than str2\n");
+	else
+		printf("str1 is equal to str2\n");
+
+	return (0);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,13 @@ int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
+
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+int print_last_digit(int n);
 #endif /* MAIN_H */
