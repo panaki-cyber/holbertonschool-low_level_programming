558d55b12eb3a61abe2f54555d73e7d028bfad64
panaki-cyber <gillotpanaki@gmail.com>
2025-10-31 18:01:05 +0000

    Feat: function that concatenates two strings.

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates the src string to the dest string
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	// Move ptr to the end of dest string
+	while (*ptr != '\0')
+		ptr++;
+
+	// Copy src to the end of dest
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	*ptr = '\0'; // Add the terminating null byte
+
+	return dest;
+}
+
+int main(void)
+{
+	char str1[50] = "Hello, ";
+	char str2[] = "world!";
+
+	_strcat(str1, str2);
+	printf("Concatenated string: %s\n", str1);
+
+	return 0;
+}
