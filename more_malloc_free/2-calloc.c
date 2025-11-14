dd74dced53f849d168d706afc79eb5c3355be7c1
panaki-cyber <gillotpanaki@gmail.com>
2025-11-14 11:55:17 +0000

    Feat: add _calloc function to allocate and zero-initialize an array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array and initializes it to 0
+ * @nmemb: number of elements
+ * @size: size of each element in bytes
+ *
+ * Return: pointer to allocated memory, or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int total_size;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
