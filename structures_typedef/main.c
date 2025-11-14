58a8a02a156d37fcb2374a3ddaee298978b8613a
panaki-cyber <gillotpanaki@gmail.com>
2025-11-14 22:08:47 +0000

    Fix: compile program

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
 
+#include <stdio.h>
+#include "dog.h"
+
 int main(void)
 {
 	struct dog my_dog;
 
 	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+	print_dog(&my_dog);
 
-	printf("Name: %s\n", my_dog.name);
-	printf("Age: %.1f\n", my_dog.age);
-	printf("Owner: %s\n", my_dog.owner);
-
-	return 0;
+	return (0);
 }
